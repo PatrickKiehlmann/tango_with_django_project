16c588d256628aee3f14352480c79cd3766a651f
PatrickKiehlmann <2623719k@student.gla.ac.uk>
2022-02-10 12:11:11 +0000

    Chapter 4 complete with tests done and my own picures added

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DOCS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
