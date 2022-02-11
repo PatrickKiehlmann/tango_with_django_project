2529669e96bb34cac009e408747716dc728a5445
PatrickKiehlmann <2623719k@student.gla.ac.uk>
2022-02-11 00:50:30 +0000

    chapter 7 complete, commit before tests are run

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, 
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, 
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -14,11 +14,21 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
+    title = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
                             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, 
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    
+    def clean(self): #made a thing to check for https but the tests might fail
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            
+        return cleaned_data
 
     class Meta:
         model = Page
